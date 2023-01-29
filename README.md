8b9b142b2e36f650d79bb422b568b9d17e291a1e
Duncan Corps <github@duncan-corps.me.uk>
2023-01-29 19:04:45 +0000

    Some actual client code and improved README.

--- README.md
+++ README.md
@@ -1,31 +1,46 @@
 # BBC-Coding-SETL-News-clock
 BBC SETL Coding test: Control and update News lower thirds clock.
 
-## Setting up
-You need Java 17.
+# Setting up
+You need a Java 17 development kit installed to compile the code. Instructions on how to do this are out-of-scope for this README, but there are several freely available builds of OpenJDK 17.
 
-## The control app
+Whichever OS you are using probably has a prebuilt package installable from its package manager.
+
+Further reading;
+* [OpenJDK](https://openjdk.org/) (general information but emphasising Oracle's build);
+  * [About OpenJDK 17](https://openjdk.org/projects/jdk/17/)
+  * [Installing](https://openjdk.org/install/) (Linux-heavy)
+* [Eclipse Temurin (AKA Adoptium)](https://projects.eclipse.org/projects/adoptium.temurin/downloads)
+
+# The control app
 To run the control app, at the command line;
-```./mvnw -DskipTests clean spring-boot:run```
-This will use default values for;
-* CasparCG Server host: localhost
-* Left tab text prefix: BBC News
+```
+./mvnw spring-boot:run
+```
+Among many other iYou should see output along the
 
-The CasparCG Server host can be specified like this;
-```
-./mvnw -DskipTests clean install spring-boot:run -Dspring-boot.run.arguments="--casparCGServer.host=ccgs01.fwei.org.uk"
-```
-
-The Left tab text prefix can be specified like this;
-```
-./mvnw -DskipTests clean install spring-boot:run -Dspring-boot.run.arguments="--leftTab.textPrefix='British Broadcasting Corporation News'"
-```
-Note that single quotes are used here because the prefix contains spaces) 
-
-Both can be specified together;
-```
-./mvnw -DskipTests clean install spring-boot:run -Dspring-boot.run.arguments="--casparCGServer.host=ccgs01.fwei.org.uk --leftTab.textPrefix='Aunty Beeb News'"
-```
-
+## Settings
+Several settings can be configured at the command line; 
+| Setting              | CLI option            | Default value | Notes                                             |
+| -------------------- | --------------------- | ------------- | ------------------------------------------------- |
+| CasparCG Server host | --casparCGServer.host | localhost     | use *null* for console output                     | 
+| CasparCG Server host | --casparCGServer.port | 5250          |                                                   |
+| Left tab text prefix | --leftTab.textPrefix  | 'BBC News'    | Use single quotes around values containing spaces |
+### Examples
+* The CasparCG Server host can be specified like this;
+  ```
+  ./mvnw spring-boot:run -Dspring-boot.run.arguments="--casparCGServer.host=ccgs01.fwei.org.uk"
+  ```
+* The Left tab's prefix text can be specified like this;
+  ```
+  ./mvnw spring-boot:run -Dspring-boot.run.arguments="--leftTab.textPrefix='British Broadcasting Corporation News'"
+  ```
+  (note that single quotes are used here because the prefix contains spaces) 
+* Multiple options can be specified together;
+  ```
+  ./mvnw spring-boot:run -Dspring-boot.run.arguments="--casparCGServer.host=ccgs01.fwei.org.uk --leftTab.textPrefix='Aunty Beeb News'"
+  ```
+## Web UI
 To use the control app, open your web browser and browse to;
-* http://localhost:8080/
+* [http://localhost:8080/](http://localhost:8080/)
+... to see some details of the app's internal state and a link to toggle the Left tab.
--- src/main/java/uk/org/fwei/casparcg/server/Client.java
+++ src/main/java/uk/org/fwei/casparcg/server/Client.java
@@ -1,17 +1,123 @@
 package uk.org.fwei.casparcg.server;
 
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+/**
+ * Very minimal CasparCG Server client. Contains almost no error handling. Uses
+ * {@link PrintWriter#format(String, Object...)} to appropriately format and
+ * write supplied AMCP commands to and {@link BufferedReader#readLine()} to read
+ * them from a {@link Socket}. Does not handle multiline responses. Does use the
+ * REQ/RES mechanism to keep commands and responses synchronised.
+ * 
+ * @implNote {@link BufferedReader#readLine()} matches the semantics of AMCP's
+ *           responses - that they're lines terminated by <b>\r\n</b>.
+ * @author dpc
+ */
 @Component
-public class Client {
+public class Client implements Closeable {
+
+	// Format REQ and RES identifiers as 6 hexadecimal digits, zero padded, to cover
+	// a wide range of values in a concise space.
+	private static final String IDENTIFIER_FORMAT = "%06x";
+	private static final AtomicInteger identifierAtomicInteger = new AtomicInteger();
+
+	private Socket socket = null;
+	private PrintWriter printWriter = null;
+	private BufferedReader bufferedReader = null;
 
 	@Value("${casparCGServer.host:localhost}")
 	private String host;
-	
-	public String send(final String amcpCommand) {
-		System.out.println("send [%s] to %s".formatted(amcpCommand, host));
 
-		return null;
+	@Value("${casparCGServer.port:5250}")
+	private int port;
+
+	private boolean connected() {
+		if ("null".equals(host)) {
+			socket = null;
+		} else if (socket == null || !socket.isConnected() || socket.isClosed()) {
+			try {
+				socket = new Socket(host, port);
+			} catch (final IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+
+		if (socket != null && socket.isConnected() && !socket.isClosed()) {
+			if (printWriter == null || bufferedReader == null) {
+				try {
+					final OutputStream outputStream = socket.getOutputStream();
+					final InputStream inputStream = socket.getInputStream();
+					final boolean autoFlush = true;
+					printWriter = new PrintWriter(outputStream, autoFlush, StandardCharsets.UTF_8);
+					final Reader reader = new InputStreamReader(inputStream);
+					bufferedReader = new BufferedReader(reader);
+				} catch (final IOException ioException) {
+					ioException.printStackTrace();
+				}
+			}
+		} else {
+			printWriter = null;
+			bufferedReader = null;
+		}
+
+		return printWriter != null && bufferedReader != null;
+	}
+
+	public String send(final String amcpCommand) {
+		String amcpResponse = null;
+
+		synchronized (identifierAtomicInteger) {
+			if (connected()) {
+				final int identifier = identifierAtomicInteger.getAndIncrement();
+				printWriter.format("REQ " + IDENTIFIER_FORMAT + " %s\r\n", identifier, amcpCommand.trim());
+				final String amcpResponsePrefix = "RES " + IDENTIFIER_FORMAT + " ".formatted(identifier);
+
+				do {
+					try {
+						amcpResponse = bufferedReader.readLine();
+					} catch (final IOException ioException) {
+						ioException.printStackTrace();
+					}
+				} while (amcpResponse != null && !amcpResponse.startsWith(amcpResponsePrefix));
+			} else {
+				System.out.println("Cannot send [%s] to %s:%d".formatted(amcpCommand, host, port));
+			}
+		}
+
+		return amcpResponse;
+	}
+
+	@Override
+	public void close() throws IOException {
+		synchronized (socket) {
+			if (socket != null && !socket.isClosed()) {
+				Stream.of(bufferedReader, printWriter, socket).filter(Objects::nonNull).forEach(closeable -> {
+					try {
+						closeable.close();
+					} catch (final IOException ioException) {
+						// Nothing to do
+					}
+				});
+
+				bufferedReader = null;
+				printWriter = null;
+				socket = null;
+			}
+		}
 	}
 }
